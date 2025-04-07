69fc0969dfd5524080c9235160a3b8f24c68dcaa
Juan Cremades <juan.cremades@emineo.es>
2025-04-07 18:08:36 +0200

    Added a FadeIn css effect to the cards

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,17 @@ module.exports = {
     fontFamily: {
       montserrat: ["Montserrat", "sans-serif"],
     },
-    extend: {},
+    extend: {
+      animation: {
+        fadeIn: "fadeIn 0.3s ease-in-out",
+      },
+      keyframes: {
+        fadeIn: {
+          "0%": { opacity: 0 },
+          "100%": { opacity: 1 },
+        },
+      },
+    },
   },
   plugins: [require("daisyui")],
   daisyui: {
