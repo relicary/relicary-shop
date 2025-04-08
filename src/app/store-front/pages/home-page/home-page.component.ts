5400da1d99a9c000dcba9e27e965d4b7682e3656
Juan Cremades <juan.cremades@emineo.es>
2025-04-08 11:38:48 +0200

    Using Path Alias without general

--- src/app/store-front/pages/home-page/home-page.component.ts
+++ src/app/store-front/pages/home-page/home-page.component.ts
@@ -1,6 +1,5 @@
-import { ProductCardComponent } from '@/products/components/product-card/product-card.component';
 import { Component } from '@angular/core';
-// import { ProductCardComponent } from '../../../products/components/product-card/product-card.component';
+import { ProductCardComponent } from '@products/components/product-card/product-card.component';
 
 @Component({
   selector: 'app-home-page',
