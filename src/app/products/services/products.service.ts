82ee265c9ae5dfc8955b2eae414ba8a94e8eac44
Juan Cremades <juan.cremades@emineo.es>
2025-04-08 12:17:01 +0200

    Added Product Service with HttpClient injection

---
+++ src/app/products/services/products.service.ts
@@ -0,0 +1,7 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+
+@Injectable({ providedIn: 'root' })
+export class ProductsService {
+  private http = inject(HttpClient);
+}
