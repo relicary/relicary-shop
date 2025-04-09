f2f978434a9ee0610a9e9ce88e994829ef697ff5
Juan Cremades <juan.cremades@emineo.es>
2025-04-09 15:39:29 +0200

    Added filter pages by Gender

--- src/app/store-front/pages/gender-page/gender-page.component.ts
+++ src/app/store-front/pages/gender-page/gender-page.component.ts
@@ -1,9 +1,26 @@
-import { Component } from '@angular/core';
+import { TitleCasePipe } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { rxResource, toSignal } from '@angular/core/rxjs-interop';
+import { ActivatedRoute } from '@angular/router';
+import { ProductCardComponent } from '@products/components/product-card/product-card.component';
+import { ProductsService } from '@products/services/products.service';
+import { map, tap } from 'rxjs';
 
 @Component({
   selector: 'app-gender-page',
-  imports: [],
+  imports: [ProductCardComponent, TitleCasePipe],
   templateUrl: './gender-page.component.html',
   styleUrl: './gender-page.component.css',
 })
-export class GenderPageComponent { }
+export class GenderPageComponent {
+  route = inject(ActivatedRoute);
+  private productsService = inject(ProductsService);
+  gender = toSignal(this.route.params.pipe(map(({ gender }) => gender)));
+
+  productsResource = rxResource({
+    request: () => ({ gender: this.gender() }),
+    loader: ({ request }) => {
+      return this.productsService.getProducts({ gender: this.gender() });
+    },
+  });
+}
