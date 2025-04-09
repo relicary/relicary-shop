80bbe0c5873812b817d28d938832a8ec43d5554a
Juan Cremades <juan.cremades@emineo.es>
2025-04-09 16:58:41 +0200

    Getting Current Page from the URL

--- src/app/store-front/pages/home-page/home-page.component.ts
+++ src/app/store-front/pages/home-page/home-page.component.ts
@@ -1,8 +1,10 @@
 import { Component, inject } from '@angular/core';
-import { rxResource } from '@angular/core/rxjs-interop';
+import { rxResource, toSignal } from '@angular/core/rxjs-interop';
+import { ActivatedRoute } from '@angular/router';
 import { ProductCardComponent } from '@products/components/product-card/product-card.component';
 import { ProductsService } from '@products/services/products.service';
 import { PaginationComponent } from '@shared/components/pagination/pagination.component';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-home-page',
@@ -12,6 +14,18 @@ import { PaginationComponent } from '@shared/components/pagination/pagination.co
 })
 export class HomePageComponent {
   private productsService = inject(ProductsService);
+
+  activatedRoute = inject(ActivatedRoute);
+
+  currentPage = toSignal(
+    this.activatedRoute.queryParamMap.pipe(
+      map((params) => (params.get('page') ? +params.get('page')! : 1)),
+      map((page) => (isNaN(page) ? 1 : page))
+    ),
+    {
+      initialValue: 1,
+    }
+  );
 
   productsResource = rxResource({
     request: () => ({}),
