5ff8ea98c1317a5f14a8b683ff92a1b9c46372dd
Juan Cremades <juan.cremades@emineo.es>
2025-04-10 13:23:07 +0200

    CurrentPage flow extradted to its own service

--- src/app/store-front/pages/home-page/home-page.component.ts
+++ src/app/store-front/pages/home-page/home-page.component.ts
@@ -1,10 +1,9 @@
 import { Component, inject } from '@angular/core';
-import { rxResource, toSignal } from '@angular/core/rxjs-interop';
-import { ActivatedRoute } from '@angular/router';
+import { rxResource } from '@angular/core/rxjs-interop';
 import { ProductCardComponent } from '@products/components/product-card/product-card.component';
 import { ProductsService } from '@products/services/products.service';
 import { PaginationComponent } from '@shared/components/pagination/pagination.component';
-import { map } from 'rxjs';
+import { PaginationService } from '@shared/components/pagination/pagination.service';
 
 @Component({
   selector: 'app-home-page',
@@ -15,20 +14,10 @@ import { map } from 'rxjs';
 export class HomePageComponent {
   private productsService = inject(ProductsService);
 
-  activatedRoute = inject(ActivatedRoute);
-
-  currentPage = toSignal(
-    this.activatedRoute.queryParamMap.pipe(
-      map((params) => (params.get('page') ? +params.get('page')! : 1)),
-      map((page) => (isNaN(page) ? 1 : page))
-    ),
-    {
-      initialValue: 1,
-    }
-  );
+  paginationService = inject(PaginationService);
 
   productsResource = rxResource({
-    request: () => ({ page: this.currentPage() - 1 }),
+    request: () => ({ page: this.paginationService.currentPage() - 1 }),
     loader: ({ request }) => {
       return this.productsService.getProducts({ offset: request.page * 9 });
     },
