5a78e5e3b3c19b6ded01dd7b622b5588973f1266
Juan Cremades <juan.cremades@emineo.es>
2025-04-10 13:54:49 +0200

    Added a cache into the Product Service to avoid several calls

--- src/app/products/services/products.service.ts
+++ src/app/products/services/products.service.ts
@@ -20,6 +20,7 @@ export class ProductsService {
   private http = inject(HttpClient);
 
   private productsCache = new Map<string, ProductsResponse>();
+  private productCache = new Map<string, Product>();
 
   getProducts(options: Options): Observable<ProductsResponse> {
     const { limit = 9, offset = 0, gender = '' } = options;
@@ -44,6 +45,13 @@ export class ProductsService {
   }
 
   getProductByIdSlug(idSlug: string): Observable<Product> {
-    return this.http.get<Product>(`${baseUrl}/products/${idSlug}`);
+    if (this.productCache.has(idSlug)) {
+      return of(this.productCache.get(idSlug)!);
+    }
+
+    return this.http.get<Product>(`${baseUrl}/products/${idSlug}`).pipe(
+      tap((product) => console.log({ response: product })),
+      tap((product) => this.productCache.set(idSlug, product))
+    );
   }
 }
