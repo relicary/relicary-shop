1a8310e379c7232b678b051f7a323beeb8c70eed
Juan Cremades <juan.cremades@emineo.es>
2025-04-11 14:22:20 +0200

    Added Login form

--- src/app/auth/pages/login-page/login-page.component.ts
+++ src/app/auth/pages/login-page/login-page.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login-page',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.css',
 })
-export class LoginPageComponent { }
+export class LoginPageComponent {}
