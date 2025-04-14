4414de7974e44a2128cc96fc4a9a1a25be7e4b8f
Juan Cremades <juan.cremades@emineo.es>
2025-04-14 18:11:37 +0200

    Added a Dashboard Navigation menu

--- src/app/admin-dashboard/layouts/admin-dashboard-layout/admin-dashboard-layout.component.ts
+++ src/app/admin-dashboard/layouts/admin-dashboard-layout/admin-dashboard-layout.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, computed, inject } from '@angular/core';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { AuthService } from '@auth/services/auth.service';
 
 @Component({
   selector: 'app-admin-layout',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './admin-dashboard-layout.component.html',
   styleUrl: './admin-dashboard-layout.component.css',
 })
-export class AdminDashboardLayoutComponent {}
+export class AdminDashboardLayoutComponent {
+  authService = inject(AuthService);
+
+  user = computed(() => this.authService.user());
+}
