dbe4cff215f52da51b5f2488af9082e59c6fe733
Juan Cremades <juan.cremades@emineo.es>
2025-04-15 11:54:32 +0200

    isAdmin guard is called from app routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -17,7 +17,6 @@ export const routes: Routes = [
   {
     path: 'admin',
     loadChildren: () => import('./admin-dashboard/admin-dashboard.routes'),
-    canMatch: [IsAdminGuard],
   },
   {
     path: '',
