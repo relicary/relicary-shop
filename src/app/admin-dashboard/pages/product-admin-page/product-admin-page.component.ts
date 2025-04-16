bafc89b3e0b490b651f5e90c1b4eb86c760e131a
Juan Cremades <relicary@gmail.com>
2025-04-16 13:31:46 +0200

    Get Product using the ID from the path

--- src/app/admin-dashboard/pages/product-admin-page/product-admin-page.component.ts
+++ src/app/admin-dashboard/pages/product-admin-page/product-admin-page.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, effect, inject } from '@angular/core';
+import { rxResource, toSignal } from '@angular/core/rxjs-interop';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ProductsService } from '@products/services/products.service';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-product-admin-page',
@@ -6,4 +10,23 @@ import { Component } from '@angular/core';
   templateUrl: './product-admin-page.component.html',
   styleUrl: './product-admin-page.component.css',
 })
-export class ProductAdminPageComponent {}
+export class ProductAdminPageComponent {
+  activatedRoute = inject(ActivatedRoute);
+  router = inject(Router);
+  productService = inject(ProductsService);
+
+  productId = toSignal(
+    this.activatedRoute.params.pipe(map((params) => params['id']))
+  );
+
+  productResource = rxResource({
+    request: () => ({ id: this.productId() }),
+    loader: ({ request }) => this.productService.getProductById(request.id),
+  });
+
+  redirectEffect = effect(() => {
+    if (this.productResource.error()) {
+      this.router.navigate(['/admin/products']);
+    }
+  });
+}
--- src/app/products/services/products.service.ts
+++ src/app/products/services/products.service.ts
@@ -54,4 +54,14 @@ export class ProductsService {
       tap((product) => this.productCache.set(idSlug, product))
     );
   }
+
+  getProductById(id: string): Observable<Product> {
+    if (this.productCache.has(id)) {
+      return of(this.productCache.get(id)!);
+    }
+
+    return this.http
+      .get<Product>(`${baseUrl}/products/${id}`)
+      .pipe(tap((product) => this.productCache.set(id, product)));
+  }
 }
