690264d4689e3b8adac087a9950b167d12659d4a
Juan Cremades <relicary@gmail.com>
2025-04-16 10:24:35 +0200

    Products are loaded in the Product Admin Page

--- src/app/admin-dashboard/pages/products-admin-page/products-admin-page.component.ts
+++ src/app/admin-dashboard/pages/products-admin-page/products-admin-page.component.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { ProductTableComponent } from '../../../products/components/product-table/product-table.component';
+import { ProductsService } from '@products/services/products.service';
+import { rxResource } from '@angular/core/rxjs-interop';
+import { PaginationService } from '@shared/components/pagination/pagination.service';
 
 @Component({
   selector: 'app-products-admin-page',
@@ -7,4 +10,18 @@ import { ProductTableComponent } from '../../../products/components/product-tabl
   templateUrl: './products-admin-page.component.html',
   styleUrl: './products-admin-page.component.css',
 })
-export class ProductsAdminPageComponent {}
+export class ProductsAdminPageComponent {
+  productService = inject(ProductsService);
+  paginationService = inject(PaginationService);
+
+  productsResource = rxResource({
+    request: () => ({
+      page: this.paginationService.currentPage() - 1,
+    }),
+    loader: ({ request }) => {
+      return this.productService.getProducts({
+        offset: request.page * 9,
+      });
+    },
+  });
+}
