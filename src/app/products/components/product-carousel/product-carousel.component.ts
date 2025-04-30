7b895e54f099294aed50d50590ac32a21673643c
Juan Cremades <relicary@gmail.com>
2025-04-30 17:24:20 +0200

    As Swiper is not an angular components, it must be updated manually when images changes

--- src/app/products/components/product-carousel/product-carousel.component.ts
+++ src/app/products/components/product-carousel/product-carousel.component.ts
@@ -3,6 +3,8 @@ import {
   Component,
   ElementRef,
   input,
+  OnChanges,
+  SimpleChanges,
   viewChild,
 } from '@angular/core';
 import { ProductImagePipe } from '@products/pipes/product-image.pipe';
@@ -20,21 +22,23 @@ import { Navigation, Pagination } from 'swiper/modules';
   templateUrl: './product-carousel.component.html',
   styleUrl: './product-carousel.component.css',
 })
-export class ProductCarouselComponent implements AfterViewInit {
+export class ProductCarouselComponent implements AfterViewInit, OnChanges {
   images = input.required<string[]>();
 
   swiperDiv = viewChild.required<ElementRef>('swiperDiv');
 
-  // const swiper = new Swiper('#swiperDiv', {
-
-  //   modules: [Navigation, Pagination],
+  swiper: Swiper | undefined = undefined;
 
   ngAfterViewInit(): void {
+    this.swipperInit();
+  }
+
+  swipperInit() {
     const element = this.swiperDiv().nativeElement;
 
     if (!element) return;
 
-    const swiper = new Swiper(element, {
+    this.swiper = new Swiper(element, {
       // Optional parameters
       direction: 'horizontal',
       loop: true,
@@ -57,5 +61,16 @@ export class ProductCarouselComponent implements AfterViewInit {
         el: '.swiper-scrollbar',
       },
     });
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['images'].firstChange) {
+      return;
+    }
+
+    if (!this.swiper) return;
+
+    this.swiper.destroy(true, true);
+    this.swipperInit();
   }
 }
